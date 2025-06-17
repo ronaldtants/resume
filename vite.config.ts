e860f5820af55ace1a9d49b589231aff21010eb4
ronaldtants <ronaldtants@gmail.com>
2025-06-17 22:46:47 +0800

    add public path

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/REPO_NAME/' : '/',
+}
